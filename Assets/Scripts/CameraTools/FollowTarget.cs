beb1b51228241e13791fcd4aa52150f011930af0
GoldenDEagle <ionufriev95@gmail.com>
2023-02-07 23:36:43 +0600

    Instantiating player through container

---
+++ Assets/Scripts/CameraTools/FollowTarget.cs
@@ -0,0 +1,31 @@
+﻿using Assets.Scripts.Characters.Player;
+using Cinemachine;
+using System.Collections;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Scripts.CameraTools
+{
+    public class FollowTarget : MonoBehaviour
+    {
+        private Transform _targetToFollow;
+        private CinemachineFreeLook _cinemachineCamera;
+
+        [Inject]
+        private void Construct(PlayerController playerController)
+        {
+            _targetToFollow = playerController.transform;
+        }
+
+        private void Awake()
+        {
+            _cinemachineCamera = GetComponent<CinemachineFreeLook>();
+        }
+
+        private void Start()
+        {
+            _cinemachineCamera.Follow = _targetToFollow;
+            _cinemachineCamera.LookAt = _targetToFollow;
+        }
+    }
+}
---
+++ Assets/Scripts/Installers/PlayerInstaller.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts.Characters.Player;
+using System.Collections;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Scripts.Installers
+{
+    public class PlayerInstaller : MonoInstaller
+    {
+        [SerializeField] private GameObject _playerPrefab;
+        [SerializeField] private Transform _playerStartPosition;
+
+        public override void InstallBindings()
+        {
+            PlayerController playerController = Container
+                .InstantiatePrefabForComponent<PlayerController>(_playerPrefab, _playerStartPosition.position, Quaternion.identity, null);
+
+            Container.Bind<PlayerController>()
+                .FromInstance(playerController)
+                .AsSingle();
+        }
+    }
+}
